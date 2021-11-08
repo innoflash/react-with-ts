0989604e30116b6a7f52ef85bc40795824a49964
Innocent Mazando <innocentmazando@gmail.com>
2021-11-08 12:05:20 +0200

    wired app routing

---
+++ src/Pages/ServerResponse.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ServerResponse: React.FC = () => {
+	return (
+		<p>This is server response</p>
+	);
+};
+
+export default ServerResponse;
---
+++ src/Pages/UserInfo.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const UserInfo: React.FC = () => {
+	return (
+		<p>This is a user info page</p>
+	);
+};
+
+export default UserInfo;
