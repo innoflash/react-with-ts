f10881a59827a6456f6c100e67995892c9edba7a
Innocent Mazando <innocentmazando@gmail.com>
2021-11-08 14:07:29 +0200

    wired server progress slice

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import serverProgressSlice, { SERVER_PROGRESS_SLICE } from './server-progress.slice';
 
 const store = configureStore({
-	reducer: {}
+	reducer: {
+		[SERVER_PROGRESS_SLICE]: serverProgressSlice.reducer
+	}
 });
 
 export default store;
+
+export type RootState = ReturnType<typeof store.getState>;
---
+++ src/store/server-progress.slice.ts
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const SERVER_PROGRESS_SLICE = 'server-progress-slice';
+
+const serverProgressSlice = createSlice({
+	name: SERVER_PROGRESS_SLICE,
+	initialState: <ServerProgressSliceModel | null>null,
+	reducers: {
+		showProgress: (state, payload) => payload as unknown as ServerProgressSliceModel,
+		hideProgress: (__, _) => null
+	}
+});
+export default serverProgressSlice;
+export const serverProgressSliceActions = serverProgressSlice.actions;
+
+export enum ServerProgressType {
+	PENDING = 'pending',
+	SUCCESS = 'success',
+	ERROR = 'error'
+}
+
+export interface ServerProgressSliceModel {
+	title: string;
+	message: string;
+	type: ServerProgressType;
+}
